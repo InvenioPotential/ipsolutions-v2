a3ed7b23e21048ba2d73a0b9d8a7f4d92bd83cec
Siti Nurazalina <s.nurazalina@Invenioptl.com>
2024-03-04 12:12:49 +0800

    littleAdjustment

--- client/app/modules/task/(route)/taskNonRecur/page.tsx
+++ client/app/modules/task/(route)/taskNonRecur/page.tsx
@@ -12,7 +12,7 @@ async function TaskNonRecur() {
   const tasks: item[] = await getTasks();
   console.log(tasks)
   return (
-      <div className='lg:pl-10 lg:pr-10 md:pl-5 sm:pl-5 md:pr-5 sm:pr-5 lg:m-5 md:m-10 sm:m-10'>
+      <div className='lg:pl-10 m-5 lg:pr-10'>
         <div className='border-b-1 mb-5 flex justify-between'>
           <div className=''>
             <div className='text-2xl text-black font-bold'>
@@ -23,15 +23,15 @@ async function TaskNonRecur() {
             </div>
           </div>
         </div>
-        <div className=' min-h-screen rounded-md p-3 text-gray-500'>
+        <div className=' min-h-screen overflow-auto rounded-md text-sm text-gray-500'>
           <div className='mx-auto overflow-auto lg:h-auto rounded-md p-2 sm:m-1 md:m-1 m-5'>
             <div className='grid grid-rows-1 lg:grid-cols-4 gap-4 md:grid-cols-1 sm:grid-cols-1'>
               <div className='overflow-auto p-2 bg-white rounded-md grid-cols-subgrid lg:col-span-3 md:col-span-2 sm:col-span-2 row-span-2'>
-                <div className='overflow-auto w-auto rounded-md text-md font-semibold p-3 m-2 text-center justify-between flex'>
+                <div className='overflow-auto w-auto rounded-md  font-semibold p-3 m-2 text-center justify-between flex'>
                   <div className=''>
                     <h6 className='text-gray-900'>SITE:</h6>
                     <div>
-                      <select className='rounded-md'>
+                      <select>
                         <option value='all'>All</option>
                         <option>HQ</option>
                         <option>PCR</option>
@@ -43,7 +43,7 @@ async function TaskNonRecur() {
                   <div className=''>
                     <h6 className='text-gray-900 '>STATUS: </h6>
                     <div>
-                      <select className='rounded-md'>
+                      <select>
                         <option>All</option>
                         <option>On-going</option>
                         <option>Completed</option>
@@ -54,8 +54,8 @@ async function TaskNonRecur() {
                   <AddNonRecur/>
                   </div>
                   <div className='overflow-auto mx-auto rounded-md text-md font-semibold p-3 m-2 text-center grid-cols-subgrid lg:col-span-3 md:col-span-2 sm:col-span-2'>
-                    <table   className='container table-auto max-w-screen bg-gradient-to-r from-green-300 via-blue-100 to-gray-200 rounded-md p-4'>
-                        <thead className='text-black '>
+                    <table  className='overflow-auto container table-auto max-w-screen bg-gradient-to-r from-green-300 via-blue-100 to-gray-200 rounded-md p-4'>
+                        <thead className='text-gray-700 '>
                             <tr className='*:p-4 justify-between'>
                                 <th>TASK</th>
                                 <th>CATEGORY</th>
@@ -83,10 +83,10 @@ async function TaskNonRecur() {
                       </table>
                   </div>
                 </div>              
-                <div className='p-2 grid rounded-md grid-cols-subgrid lg:col-span-1 md:col-span-2 sm:col-span-2 row-span-1 bg-white'>
+                <div className='hover:shadow p-2 grid rounded-md grid-cols-subgrid lg:col-span-1 md:col-span-2 sm:col-span-2 row-span-1 bg-white'>
                   <AssignTask/>
                 </div>
-                <div className='p-2 rounded-md grid-cols-subgrid lg:col-span-1 md:col-span-2 sm:col-span-2 row-span-1 bg-white'>
+                <div className='hover:shadow p-2 rounded-md grid-cols-subgrid lg:col-span-1 md:col-span-2 sm:col-span-2 row-span-1 bg-white'>
                     <MiniCalendar />
                 </div>
             </div>
---
+++ client/components/dateCalendar/dateRangeReport.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React, { useState } from "react";
+import DatePickers from "./datepickers";
+import dayjs, { Dayjs } from "dayjs";
+
+const DateRangeReport = () => {
+    const [dateInput, setDateInput] = useState<Dayjs | null>(dayjs());
+
+    const handleDateInput = (date: any) => {
+        setDateInput(date);
+    };
+
+    const [dateInput2, setDateInput2] = useState<Dayjs | null>(dayjs());
+
+    const handleDateInput2 = (date: any) => {
+        setDateInput2(date);
+    };
+    return (
+        <div className="">
+            <div className="justify-between flex *:m-2">
+                <div>
+                    <div>Start</div>
+                    <DatePickers setDateInput={handleDateInput} />
+                </div>
+                <div>
+                    <div>End</div>
+                    <DatePickers setDateInput={handleDateInput2} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DateRangeReport;
